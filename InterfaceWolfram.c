f50fe5d229fd42ac19343bdec88f2b95a5d80e78
pedrohnv <pedrohnv@hotmail.com>
2019-01-04 22:13:35 -0200

    Use mur in the wolfram interface.

--- InterfaceWolfram.c
+++ InterfaceWolfram.c
@@ -32,7 +32,7 @@ DLLEXPORT int Mcalculate_impedances(
     MTensor electensor = MArgument_getMTensor(Args[0]);
     mcomplex gamma = MArgument_getComplex(Args[1]);
     mcomplex s = MArgument_getComplex(Args[2]);
-    mreal mu = MU0*MArgument_getReal(Args[3]);
+    mreal mur = MArgument_getReal(Args[3]);
     mcomplex kappa = MArgument_getComplex(Args[4]);
     mint max_eval = MArgument_getInteger(Args[5]);
     mreal req_abs_error = MArgument_getReal(Args[6]);
@@ -66,7 +66,7 @@ DLLEXPORT int Mcalculate_impedances(
     _Complex double gamma1 = gamma.ri[0] + I*gamma.ri[1];
     _Complex double kappa1 = kappa.ri[0] + I*kappa.ri[1];
     _Complex double s1 = s.ri[0] + I*s.ri[1];
-    calculate_impedances(electrodes, num_electrodes, zl, zt, gamma1, s1, mu,
+    calculate_impedances(electrodes, num_electrodes, zl, zt, gamma1, s1, mur,
             kappa1, max_eval, req_abs_error, req_rel_error, error_norm,
             integration_type);
     MTensor zlzt;
@@ -120,7 +120,7 @@ DLLEXPORT int Mimpedances_images(
     MTensor electensor = MArgument_getMTensor(Args[0]);
     mcomplex gamma = MArgument_getComplex(Args[1]);
     mcomplex s = MArgument_getComplex(Args[2]);
-    mreal mu = MU0*MArgument_getReal(Args[3]);
+    mreal mur = MArgument_getReal(Args[3]);
     mcomplex kappa = MArgument_getComplex(Args[4]);
     mint max_eval = MArgument_getInteger(Args[5]);
     mreal req_abs_error = MArgument_getReal(Args[6]);
@@ -171,7 +171,7 @@ DLLEXPORT int Mimpedances_images(
     _Complex double ref_t1 = ref_t.ri[0] + I*ref_t.ri[1];
     _Complex double s1 = s.ri[0] + I*s.ri[1];
     impedances_images(
-            electrodes, images, num_electrodes, zl, zt, gamma1, s1, mu, kappa1,
+            electrodes, images, num_electrodes, zl, zt, gamma1, s1, mur, kappa1,
             ref_l1, ref_t1, max_eval, req_abs_error, req_rel_error, error_norm,
             integration_type);
     MTensor zlzt;
