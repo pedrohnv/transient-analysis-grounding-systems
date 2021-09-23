3d5b16d794ae9c1f2cd932290a168716f644f704
pedrohnv <pedrohnv@hotmail.com>
2021-09-23 19:08:16 -0300

    Correct errors on plotting routine of 3 grids

--- examples/plot_results/plot_three_grids.jl
+++ examples/plot_results/plot_three_grids.jl
@@ -130,32 +130,32 @@ for k in [1, 70, 80, 90]
     for rho in [100, 1000]
         # integral of the E_C field ========================================
         df = DataFrame(CSV.File("three_grids_time_efield_$(rho)$(connect).csv"))
-        f = sort(unique(df.f))
-        nf = length(f)
+        t = sort(unique(df.t))
+        nt = length(t)
         x = sort(unique(df.x))
         dx = x[2] - x[1]
-        c = isapprox.(df.f, f[k])
-        ef = parse.(ComplexF64, df.ecx1[c])
+        c = isapprox.(df.t, t[k])
+        ef = df.ecx1[c]
         N = Int(1 ÷ dx)
         nx = length(x) - N
-        dv1 = zeros(ComplexF64, nx)
+        dv1 = zeros(Float64, nx)
         for i = 1:nx
             s = sum(ef[(i + 1):(i + N - 1)])
             dv1[i] = dx * (s + (ef[i] + ef[i + N]) / 2)
         end
         x1 = x[1:end-N]
-        plot!(p,x1, abs.(dv1), label="pot. dif., σ = $(1/rho * 1e3) mS",
+        plot!(p, x1, dv1, label="pot. dif., σ = $(1/rho * 1e3) mS",
               color=colors[line], linestyles=linestyles[line])
         line += 1
         # integral of the E_tot field ========================================
-        ef = parse.(ComplexF64, df.ecx1[c]) + parse.(ComplexF64, df.encx1[c])
-        dv2 = zeros(ComplexF64, nx)
+        ef = df.ecx1[c] + df.encx1[c]
+        dv2 = zeros(Float64, nx)
         for i = 1:nx
             s = sum(ef[(i + 1):(i + N - 1)])
             dv2[i] = dx * (s + (ef[i] + ef[i + N]) / 2)
         end
         x2 = x[1:end-N]
-        plot!(p, x2, abs.(dv2), label="voltage, σ = $(1/rho * 1e3) mS",
+        plot!(p, x2, dv2, label="voltage, σ = $(1/rho * 1e3) mS",
               color=colors[line], linestyles=linestyles[line])
         line += 1
     end
